467c7ce30e68e376be6bca4c51ccd8059ed0991b
Robert Lundberg <robert.l@perceptilabs.com>
2022-12-14 17:41:13 +0100

    added examples

--- application/model_application.py
+++ application/model_application.py
@@ -22,6 +22,15 @@ def predict(distance_from_home,distance_from_last_transaction,ratio_to_median_pu
     return "Fraud" if model.predict(np.array([[distance_from_home,distance_from_last_transaction,ratio_to_median_purchase_price,repeat_retailer,used_chip,used_pin_number,online_order]], dtype=np.float64))[0][0] >=0.995 else "Not fraud"
 
 
-demo = gr.Interface(fn=predict, inputs=["number","number","number","number","number","number","number"], outputs="textbox")
+demo = gr.Interface(
+    fn=predict, 
+    inputs=["number","number","number","number","number","number","number"], 
+    outputs="textbox",
+    examples=[
+        [57.87785658389723,0.3111400080477545,1.9459399775518593,1.0,1.0,0.0,0.0],
+        [10.664473716016785,1.5657690862016613,4.886520843107555,1.0,0.0,0.0,1.0]
+        ],
+    title="Predict Credit Fraud"
+    )
 
 demo.launch(server_name=GRADIO_SERVER_NAME, server_port=GRADIO_SERVER_PORT)
