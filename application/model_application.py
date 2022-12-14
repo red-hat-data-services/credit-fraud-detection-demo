5989a5bef4ff66939295e64e7409806e3dd48282
Robert Lundberg <robert.l@perceptilabs.com>
2022-12-14 12:08:31 +0100

    restructured

--- application/model_application.py
+++ application/model_application.py
@@ -1,7 +1,11 @@
+import os
 import mlflow.pyfunc
 import gradio as gr
+import numpy as np
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+MLFLOW_ROUTE = os.getenv("MLFLOW_ROUTE")
+
+mlflow.set_tracking_uri(MLFLOW_ROUTE)
 
 model_name = "DNN-credit-fraud"
 model_version = 1
@@ -13,7 +17,7 @@ model = mlflow.pyfunc.load_model(
 
 
 def predict(distance_from_home,distance_from_last_transaction,ratio_to_median_purchase_price,repeat_retailer,used_chip,used_pin_number,online_order):
-    return model.predict([[distance_from_home,distance_from_last_transaction,ratio_to_median_purchase_price,repeat_retailer,used_chip,used_pin_number,online_order]])[0][0]
+    return model.predict(np.array([[distance_from_home,distance_from_last_transaction,ratio_to_median_purchase_price,repeat_retailer,used_chip,used_pin_number,online_order]], dtype=np.float64))[0][0]
 
 
 demo = gr.Interface(fn=predict, inputs=["number","number","number","number","number","number","number"], outputs="number")
